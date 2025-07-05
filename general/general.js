3b82b450983be8896f32e66388e7f76a0508f46b
Ani-TW <buzhubu98@gmail.com>
2025-07-05 15:22:01 +0800

    加上nav開啟時左邊的遮罩

--- general/general.js
+++ general/general.js
@@ -6,14 +6,15 @@ function burgerList(){
     let burger = document.querySelector('#burger')
     let nav = document.querySelector('nav')
     
+    //漢堡開關
     burger.addEventListener('click',function(e){
         nav.classList.toggle('show')
     }) 
 
-    let wrapper = document.querySelector('.wrapper')
-
-    wrapper.addEventListener('click',()=>{
+    //nav左邊的遮罩 避免按到後面的連結
+    mask.addEventListener('click',()=>{
         nav.classList.remove('show')
+        mask.classList.remove('mask')
     })
 }
     
